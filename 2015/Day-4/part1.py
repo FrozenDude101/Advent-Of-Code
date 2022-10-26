ac5f929316b52cbfd7980cfee4d09964c89cd8b4
Matthew Hammond <FrozenDude101@outlook.com>
2022-10-26 17:06:06 +0100

    2015, Day 4

---
+++ 2015/Day-4/part1.py
@@ -0,0 +1,23 @@
+from sys import path
+path.append("../..")
+
+from AoC import *
+
+import hashlib
+
+def run(data):
+
+    i = 0
+    while True:
+        hashText = hashlib.md5((data + str(i)).encode("utf-8")).hexdigest()
+        if hashText[0:5] == "00000":
+            break
+        i+=1
+
+    return i
+
+with open("input.txt") as f:
+    data = f.read()
+
+ret = run(data)
+print(ret)
