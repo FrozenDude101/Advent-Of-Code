e4752388e2ccfbae3241b1665da2ef599584a356
Matthew Hammond <FrozenDude101@outlook.com>
2022-10-30 18:44:53 +0000

    2015 Day 6

---
+++ 2015/Day-6/part2.py
@@ -0,0 +1,51 @@
+from sys import path
+path.append("../..")
+
+from AoC import *
+
+import hashlib
+
+def format(line):
+
+    line = line.split(" ")
+
+    ret = {}
+    ret["action"] = line[0]
+    ret["start"] = tuple(map(int, line[1].split(",")))
+    ret["end"] = tuple(map(int, line[3].split(",")))
+
+    return ret
+
+def run(data):
+
+    data = splitOn(data, "\n")
+    data = list(map(lambda x: x.replace("turn ", ""), data))
+    data = list(map(format, data))
+
+    field = [[0 for i in range(1000)] for j in range(1000)]
+
+    for ins in data:
+        print(ins)
+        action = ins["action"]
+        start = ins["start"]
+        end = ins["end"]
+        for i in range(start[0], end[0]+1):
+            for j in range(start[1], end[1]+1):
+                if action == "off" and field[i][j] != 0:
+                    field[i][j] -= 1
+                if action == "on":
+                    field[i][j] += 1
+                if action == "toggle":
+                    field[i][j] += 2
+
+    on = 0
+    for line in field:
+        for light in line:
+            on += light
+    return on
+
+with open("input.txt") as f:
+    data = f.read()
+
+ret = run(data)
+print(ret)
