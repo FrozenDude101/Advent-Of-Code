c91b5712af66abc2428b4bfdf093b53893ad01e1
Matthew Hammond <FrozenDude101@outlook.com>
2022-12-01 17:43:05 +0000

    2022 Day 1, and added a grouped function that parses data grouped by \n\n into a list of lists, with optional retyping.

--- AoC/mapping.py
+++ AoC/mapping.py
@@ -15,3 +15,28 @@ def splitOn(value, splitChar):
         return list(map(lambda val: splitOn(val, splitChar), value))
 
     return value.split(splitChar)
+
+
+def splitMultiple(value, splits, types = None):
+
+    value = [None, value]
+    ret = []
+
+    for i in range(len(splits)):
+        value = value[1].split(splits[i])
+        if types != None and len(types) > i:
+            ret.append(types[i](value[0]))
+        else:
+            ret.append(value[0])
+
+    return ret
+
+
+def grouped(value, type = None):
+
+    ret = splitOn(splitOn(value, "\n\n"), "\n")
+
+    if type is not None:
+        ret = mapAll(ret, type)
+
+    return ret
